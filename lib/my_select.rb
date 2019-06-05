9d2543d21b907d4d540c627b23ab0e489723c984
magical-flow-3186 <rregalado17@gmail.com>
2019-06-05 16:33:13 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,8 @@
 def my_select(collection)
   i = 0 
   new = []
-  while i < collection[i]) == true 
+  while i < collection.length 
+  if yield(collection[i]) == true 
   new << collection[i]
 end
 i = i + 1 
