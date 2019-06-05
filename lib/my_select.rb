1340f79e682a06de63bffd541a22ca743a3944ad
magical-flow-3186 <rregalado17@gmail.com>
2019-06-05 16:33:21 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i = 0 
+  new = []
+  while i < collection.length 
+  if yield(collection[i]) == true 
+  new << collection[i]
+end
+i = i + 1 
+end
+new
 end
